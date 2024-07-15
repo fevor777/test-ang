0e0406afc97c36aa55ee3afadf79bda9b14dc886
Viktar Hrynko <vhrynko@infoblox.com>
2024-07-15 16:51:15 +0300

    some fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test-build-ang';
+  title!: string;
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('https://test-nodejs-production-ce92.up.railway.app/hello').subscribe((data: any) => {
+      this.title = data?.['value'];
+    });
+  }
 }
